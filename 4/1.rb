4763f6c9df0549755284b87a87a649823e4f7bc6
Casey Kolderup <casey@kolderup.org>
2018-12-03 23:15:15 -0800

    day 4 - at least DRY up the file parsing

--- 4/1.rb
+++ 4/1.rb
@@ -1,37 +1,6 @@
-require 'date'
+require_relative 'parse'
 
-format = /\[(?<date>\d{4}-\d{2}-\d{2} \d{2}:\d{2})\] (?<action>.*)/
-
-log = IO.readlines('input.txt').map(&:strip)
-
-raw = log.map do |line|
-  result = format.match(line)
-  {
-    time: DateTime.parse(result[:date]),
-    action: result[:action]
-  }
-end
-
-# sort by time
-raw.sort_by! { |record| record[:time] }
-
-# reduce to guard + array of minutes asleep ranges
-cur_guard = -1
-sleeping = {}
-raw.each do |record|
-  if found = /Guard \#(?<id>\d+) begins shift/.match(record[:action])
-    cur_guard = found[:id].to_i
-  elsif record[:action] == 'falls asleep'
-    sleeping[cur_guard] ||= []
-    sleeping[cur_guard] << [record[:time].minute]
-  elsif record[:action] == 'wakes up'
-    start = sleeping[cur_guard].last.last + 1
-    finish = record[:time].minute - 1
-    sleeping[cur_guard].last.concat(((start)..finish).to_a)
-  else
-    exit('oh no')
-  end
-end
+sleeping = parse('input.txt')
 
 slept_amount = sleeping.map do |guard, minutes|
   { id: guard, amount: minutes.flatten.size }
