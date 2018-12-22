4ee075c59ced8067eea3da1473596af2e0046765
Casey Kolderup <casey.kolderup@voxmedia.com>
2018-12-21 16:07:46 -0800

    days 9 and 10

---
+++ 10/1.rb
@@ -0,0 +1,76 @@
+  FORMAT = /position=<\s*(?<x>-?\d+),\s*(?<y>-?\d+)> velocity=<\s*(?<vx>-?\d+),\s*(?<vy>-?\d+)>/
+
+class Point
+  def initialize(x, y, vx, vy)
+    @x = x
+    @y = y
+    @vx = vx
+    @vy = vy
+  end
+
+  def pos_at(t)
+    [@x + (t * @vx), @y + (t * @vy)]
+  end
+end
+
+def grid_at(points, t)
+  points_at = points.map { |p| p.pos_at(t) }
+  minx = points_at.map(&:first).min
+  miny = points_at.map(&:last).min
+
+  if minx != 0
+    points_at.each { |p| p[0] -= minx }
+  end
+
+  if miny != 0
+    points_at.each { |p| p[1] -= miny }
+  end
+
+  maxx = points_at.map(&:first).max + 1
+  maxy = points_at.map(&:last).max + 1
+
+  if maxy < 12
+    # this is a candidate, actually construct it
+    puts "found! constructing..."
+    grid = Array.new(maxy, [])
+    maxy.times.each do |y|
+      grid[y] = Array.new(maxx, '.')
+    end
+
+    puts "width: #{maxx}"
+    points_at.each_with_index do |point, idx|
+      grid[point.last][point.first] = 'x'
+      puts "#{idx}/#{points_at.size}"
+    end
+
+    grid
+  else
+    []
+  end
+end
+
+def draw_grid(grid)
+  grid.each do |line|
+    puts line.join('')
+  end
+end
+
+input = File.readlines('input.txt').map(&:strip).map do |line|
+  result = FORMAT.match(line)
+  Point.new(result[:x].to_i,
+            result[:y].to_i,
+            result[:vx].to_i,
+            result[:vy].to_i)
+end
+
+puts "loaded! running..."
+
+i = 0
+loop do
+  result = grid_at(input, i)
+  if result.size > 0
+    draw_grid(result)
+    break
+  end
+  i += 1
+end
---
+++ 9/2.rb
@@ -0,0 +1,41 @@
+def play(num_players, up_to)
+  scores = Array.new(num_players, 0)
+  circle = [0]
+  previous = 0
+
+  (1..up_to).each do |marble|
+
+    if (marble % 23 == 0)
+      player = marble % num_players
+      now = (previous - 7) % circle.size
+      scores[player] ||= 0
+      scores[player] += marble + circle.delete_at(now)
+      previous = now
+    else
+      now = (previous + 2) % circle.size
+      circle.insert(now, marble)
+      previous = now
+    end
+  end
+
+  scores.max
+end
+
+examples = File.readlines('examples.txt').map do |x|
+  x.chomp.split(';').map(&:to_i)
+end
+
+input = File.readlines('input2.txt').map do |x|
+  x.chomp.split(';').map(&:to_i)
+end
+
+examples.each do |game|
+  high_score = play(game[0], game[1])
+  puts "#{game[2] == high_score ? 'PASS' : 'FAIL'}: " +
+       "expected #{game[2]}, got #{high_score}"
+end
+
+input.each do |game|
+  high_score = play(game[0], game[1])
+  puts "high score: #{high_score}"
+end
